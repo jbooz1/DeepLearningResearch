a42d8a01ce0cf1860059d2a886f6b12d7ad118dd
Jarrett Booz <jarrettbooz24@gmail.com>
2017-10-25 19:52:14 -0400

    First Commit

---
+++ Code/KTFBinClass.py
@@ -0,0 +1,218 @@
+import numpy as np
+import pandas
+import sklearn
+import random
+from keras import losses
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from timeit import default_timer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import OneHotEncoder
+
+
+
+def main():
+	
+
+	good_path = './Data/goodPermissionsFinal.txt'
+	mal_path = './Data/malwarePermissionsFinal.txt'
+	
+	with open(good_path) as f:
+		gdprm = f.readlines()
+	with open(mal_path) as f:
+		mlprm = f.readlines()
+	
+	perms = gdprm + mlprm	
+	
+	labels = np.array([])
+	for x in gdprm:
+		labels = np.append(labels, 0)
+	for x in mlprm:
+		labels = np.append(labels, 1)
+
+
+	count_vect = CountVectorizer(input=u'content', analyzer=u'word', token_pattern='(\\b(:?uses-|optional-)?permission:\s[^\s]*)')
+	time0 = timeit.default_timer()
+	features = count_vect.fit_transform(perms)
+	features = features.todense()
+	features = np.array(features)
+	inputSize = len(features)
+	
+	
+
+
+	print("Done Vectorizing Data")
+	#print 'ouput: train_ratio, epochs, batch_size, avg_acc, avg_true_pos, avg_true_neg, avg_fpos_rate, avg_fneg_rate, avg_train_time, avg_test_time'
+	
+	print "Grid Search for One Layer"
+	grid_search_EpochBatch("oneLayer", features, labels)
+	print "Grid Search for Binary Decreasing Layers"
+	grid_search_EpochBatch("binaryDecrease", features, labels)
+	print "Grid Search for Four equal Layers"
+	grid_search_EpochBatch("fourSame", features, labels)
+	print "Grid Search for Four Decr Layers"
+	grid_search_EpochBatch("fourDecr", features, labels)
+	
+	return 0
+
+	
+def grid_search_EpochBatch(modelName, features, labels):
+	epochs = [1, 2, 4, 8, 16, 32]
+	batch_size = [10, 100, 200, 500, 1000, 1500, 2000, 5000]
+	paramGrid = dict(epochs=epochs, batch_size=batch_size)
+	test_ratio=.8
+		
+	
+	if modelName == "oneLayer" :
+		model = KerasClassifier(build_fn=create_one_layer, verbose=0)
+	elif modelName == "binaryDecrease":
+		model = KerasClassifier(build_fn=create_binaryDecrease, verbose=0)
+	elif modelName == "fourSame":
+		model = KerasClassifier(build_fn=create_fourSameLayer, verbose=0)
+	elif modelName == "fourDecr":
+		model = KerasClassifier(build_fn=create_fourDecrLayer, verbose=0)
+	
+	
+	sss = StratifiedShuffleSplit(n_splits=1, test_size=test_ratio, random_state=0)
+	grid = GridSearchCV(estimator=model, param_grid=paramGrid, n_jobs=1, cv=sss, refit=True, verbose=2)
+	grid_fit = grid.fit(features, labels)
+	
+	means = grid_fit.cv_results_['mean_test_score']
+	stds = grid_fit.cv_results_['std_test_score']
+	params = grid_fit.cv_results_['params']
+	
+	print("%s Best: %f using %s" % (modelName, grid_fit.best_score_, grid_fit.best_params_))
+	
+	df = pandas.DataFrame(grid_fit.cv_results_)
+	path1 = '/home/lab309/pythonScripts/testResults/deep_results/epochBatchTrain' + modelName + '.csv'
+	file1 = open(path1, "w+")
+	df.to_csv(file1, index=True)
+	file1.close()
+	
+	return 0
+	
+	
+	
+def cross_val(modelName, features, labels, length, test_ratio, splits, tensorSize, epoch, batch):
+	
+	sss = StratifiedShuffleSplit(n_splits=1, test_size=test_ratio, random_state=0)
+	if modelName == "one" :
+		print "\nOne layer with size %d" % tensorSize
+		model = KerasClassifier(build_fn=pickle.loads(create_one_layer)(tensorSize), epochs=epoch, batch_size=batch, verbose=0)
+	elif modelName == "binaryDecrease":
+		print "\nbinary decr layers with size %d" % tensorSize
+		model = KerasClassifier(build_fn=pickle.loads(create_binaryDecrease)(tensorSize), epochs=epoch, batch_size=batch, verbose=0)
+	elif modelName == "fourSame":
+		print "\nfour layers with size %d" % tensorSize
+		model = KerasClassifier(build_fn=pickle.loads(create_fourSameLayer)(tensorSize), epochs=epoch, batch_size=batch, verbose=0)
+	elif modelName == "fourDecr":
+		print "\nfour decr layers with size %d" % tensorSize
+		model = KerasClassifier(build_fn=pickle.loads(create_fourDecrLayer)(tensorSize), epochs=epoch, batch_size=batch, verbose=0)
+		
+	start_time = time.time()
+	results = cross_val_score(model, features, labels, cv=sss, scoring='accuracy')	
+	
+	print(1-test_ratio, results.mean(), (time.time()-start_time)/splits)
+
+	
+	
+def standard_test():
+	epoch = 5
+	batch=1000
+	ratios = [.2, .4, .6, .8]
+	
+	for r in ratios:
+		#estimator = KerasClassifier(build_fn=create_one_layer(), nb_epoch=epoch, batch_size=batch, verbose=2)
+		#model = create_one_layer()
+		#test_model("one", features, labels, r, epoch, batch)
+		cross_val("one", features, labels, inputSize, r, 5, 25, epoch, batch)
+		
+	for r in ratios:
+		#estimator = KerasClassifier(build_fn=create_binaryDecrease(), nb_epoch=epoch, batch_size=batch, verbose=2)
+		#model = create_binaryDecrease()
+		#test_model("binaryDecrease", features, labels, r, epoch, batch)
+		cross_val("binaryDecrease", features, labels,inputSize, r, 5, 25, epoch, batch)
+		
+	for r in ratios:
+		#estimator = KerasClassifier(build_fn=create_four5kLayer(), nb_epoch=epoch, batch_size=batch, verbose=2)
+		#model = create_four5kLayer()
+		#test_model("four5k", features, labels, r, epoch, batch)
+		cross_val("fourSame", features, labels,inputSize, r, 5, 25, epoch, batch)
+		
+	for r in ratios:
+		#estimator = KerasClassifier(build_fn=create_fourDecrLayer(), nb_epoch=epoch, batch_size=batch, verbose=2)
+		#model = create_fourDecrLayer()
+		#test_model("fourDecr", features, labels, r, epoch, batch)
+		cross_val("fourDecr", features, labels,inputSize, r, 5, 25, epoch, batch)
+		
+	return 0
+	
+	
+def create_one_layer(size=25):
+	#baseline Model
+	model = Sequential()
+	#The first param in Dense is the number of neurons in the first hidden layer
+	model.add(Dense(size, input_dim=22300, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+	
+def create_binaryDecrease(size=25):
+	model = Sequential()
+	#The first param in Dense is the number of neurons in the first hidden layer
+	model.add(Dense(size, input_dim=22300, kernel_initializer='normal', activation='relu'))
+	while (size/2 >=1):
+		model.add(Dense(size/2, kernel_initializer='normal', activation='relu'))
+		size/=2
+	model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+	
+def create_fourSameLayer(size=25):
+	#baseline Model
+	model = Sequential()
+	#The first param in Dense is the number of neurons in the first hidden layer
+	model.add(Dense(size/3, input_dim=22300, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(size/3, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(size/3, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(size/3, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+	
+def create_fourDecrLayer(size=25):
+	#baseline Model
+	model = Sequential()
+	#The first param in Dense is the number of neurons in the first hidden layer
+	model.add(Dense(size, input_dim=22300, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(size/2, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(size/3, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(size/4, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+	
+	
+	
+if __name__ == "__main__":
+	main()
+	
---
+++ Code/apk_analysis.py
@@ -0,0 +1,193 @@
+
+import sklearn
+import numpy as np
+import timeit
+from timeit import default_timer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+
+def main():
+    '''
+    Notes for running:
+    - written for python 2.7 -> change print statements if using 3
+    - required deps -> install scikit learn (google it)
+    - edit filepaths
+    '''
+    good_path = '/home/josh/Documents/COSC/research/ml_malware/malware_data/goodPermissionsFinal.txt'
+    mal_path = '/home/josh/Documents/COSC/research/ml_malware/malware_data/malwarePermissionsFinal.txt'
+
+    with open(good_path) as f:
+        gdprm = f.readlines()
+    with open(mal_path) as f:
+        mlprm = f.readlines()
+
+    perms = gdprm + mlprm
+
+    labels = np.array([])
+    for x in gdprm:
+        labels = np.append(labels, 0)
+    for x in mlprm:
+        labels = np.append(labels, 1)
+
+
+    count_vect = CountVectorizer(input=u'content', analyzer=u'word', token_pattern='name=\'[^\']*\'')
+    time0 = timeit.default_timer()
+    features = count_vect.fit_transform(perms)
+    time1 = timeit.default_timer() #time to tokenize
+    print type(features)
+    print features.get_shape()
+    #print count_vect.get_feature_names()
+    print 'tokenize time: ' + str(time1-time0)
+    print '\n'
+
+    #proportion of data to test on vs total
+    ratios = [.8, .6, .4, .2]
+
+    print "BernoulliNB"
+    for x in ratios:
+        BNclf = BernoulliNB()
+        test_model(BNclf, features, labels, x)
+        print'\n'
+    print '---------------------------\n'
+    print "MultiNomialNB"
+    for x in ratios:
+        NBclf = MultinomialNB()
+        test_model(NBclf, features, labels, x)
+        print '\n'
+    print '---------------------------\n'
+    print "DecisionTree"
+    for x in ratios:
+        DTclf = DecisionTreeClassifier(min_samples_split = 20)
+        test_model(DTclf, features, labels, x)
+        print '\n'
+    print '---------------------------\n'
+    print "LogisticRegression"
+    for x in ratios:
+        LRclf = LogisticRegression(C=10, solver='lbfgs')
+        test_model(LRclf, features, labels, x)
+        print '\n'
+
+
+    #print "SVM"
+    #SVclf = SVC(C=10.0, kernel='rbf')
+    #SVclf.fit(features_train, labels_train)
+    #pred3 = SVclf.predict(features_test)
+    #acc3 = accuracy_score(labels_test, pred3)
+
+    #print acc3
+    ''' alternative to shuffle_split
+    print "stratifiedKFolds"
+    skf = StratifiedKFold(n_splits=5, shuffle=True)
+    for train_index, test_index in skf.split(features, labels):
+        X_train, X_test = features[train_index], features[test_index]
+        y_train, y_test = labels[train_index], labels[test_index]
+
+        BNclf = BernoulliNB()
+        BNclf.fit(X_train, y_train)
+        pred = BNclf.predict(X_test)
+        print accuracy_score(y_test, pred)
+    '''
+
+def test_model(model, features, labels, test_size):
+    '''
+    recieves an instance of an untrained model plus features and labels. performs
+    training on 5 splits of data at ratio specified by test size.
+    '''
+    print 'training ratio: ' + str(1-test_size)
+    sss = StratifiedShuffleSplit(n_splits=5, test_size=test_size)
+    avg_acc = 0.0
+    avg_true_pos = 0.0
+    avg_true_neg = 0.0
+    avg_fpos = 0.0
+    avg_fneg = 0.0
+    avg_train_time = 0.0
+    avg_test_time = 0.0
+    for train_index, test_index in sss.split(features, labels):
+        X_train, X_test = features[train_index], features[test_index]
+        y_train, y_test = labels[train_index], labels[test_index]
+
+        time0 = timeit.default_timer()
+        model.fit(X_train, y_train)
+        time1 = timeit.default_timer()
+        pred = model.predict(X_test)
+        time2 = timeit.default_timer()
+        conf_mat = confusion_matrix(y_test, pred)
+        test_len = len(y_test)
+        correct = conf_mat[0][0] + conf_mat[1][1]
+        f_pos = conf_mat[0][1]
+        f_neg = conf_mat[1][0]
+        avg_acc += float(correct)/test_len
+        avg_true_pos += conf_mat[0][0]
+        avg_true_neg += conf_mat[1][1]
+        avg_fpos += float(f_pos)/test_len
+        avg_fneg += float(f_neg)/test_len
+        avg_train_time += time1-time0
+        avg_test_time += time2-time1
+        #print conf_mat
+        ''' more verbose output
+        print 'samples tested: ' + str(test_len)
+        print 'correct predictions: ' + str(correct)
+        print 'false positives: ' + str(f_pos)
+        print 'false negatives: ' + str(f_neg)
+        print '--------'
+        print 'acc rate: ' + str(float(correct)/test_len)
+        print 'false positive rate: ' + str(float(f_pos)/test_len)
+        print 'false negative rate: ' + str(float(f_neg)/test_len)
+        print '\n\n'
+        '''
+    avg_acc = avg_acc/5
+    avg_true_pos = avg_true_pos/5
+    avg_true_neg = avg_true_neg/5
+    avg_fpos = avg_fpos/5
+    avg_fneg = avg_fneg/5
+    avg_train_time = avg_train_time/5
+    avg_test_time = avg_test_time/5
+    print 'accuracy measures: '
+    print 'avg acc: ' + str(avg_acc)
+    print 'avg_true_pos: ' + str(avg_true_pos)
+    print 'avg_true_neg: ' + str(avg_true_neg)
+    print 'avg fpos: ' + str(avg_fpos)
+    print 'avg fneg: ' + str(avg_fneg)
+
+    print '\nruntime measures: '
+    print 'avg train time: ' + str(avg_train_time)
+    print 'avg test time: ' + str(avg_test_time)
+    print '\n'
+    return
+
+#below function not used
+def full_results(labels, predictions):
+    i = 0
+    correct = 0
+    false_pos = 0
+    false_neg = 0
+    results = acc_stats()
+    while(i < len(labels)):
+        if(labels[i] == predictions[i]):
+            results.inc_correct()
+        elif(labels[i] == 1 and predictions[i] == 0):
+            results.inc_fneg()
+        elif(labels[i] == 0 and predictions[i] == 1):
+            results.inc_fpos()
+        else:
+            print 'shouldnt happen'
+        i+=1
+    results.print_stats()
+
+
+if __name__ == "__main__":
+    main()
