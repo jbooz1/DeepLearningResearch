f8ebd7aef1a420b205ac3da50d6886e61ed6afa2
Joshua <jmcgif1@students.towson.edu>
2018-03-06 12:26:51 -0500

    initial grid working - no saved output

--- Classification/KTF_dual_input.py
+++ Classification/KTF_dual_input.py
@@ -2,11 +2,15 @@ import numpy as np
 import timeit
 import pandas
 import datetime
+import argparse
 
 from functools import partial
 from nltk.tokenize.regexp import regexp_tokenize
 from sklearn.model_selection import StratifiedShuffleSplit, cross_validate, GridSearchCV, StratifiedKFold
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix
+from .keras_models import create_one_layer, create_dualInputSimple, create_dualInputLarge
+
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.layers import Merge, Dense, Dropout, Input, concatenate
 from keras.models import Model
@@ -16,46 +20,20 @@ from keras.optimizers import Nadam
 
 def main():
 
-    data_dir = '/home/jmcgiff/Documents/research/DeepLearningResearch/Data/badging_med/'
-    ben_data = data_dir + 'ben_badging_med.txt'
-    mal_data = data_dir + 'mal_badging_med.txt'
+    args = parse_arguments()
 
-    inputs = vectorize(ben_data, mal_data)
-    perm_inputs = inputs[0]
-    feat_inputs = inputs[1]
-    comb_inputs = inputs[2]
-
-    print perm_inputs.shape
-    print feat_inputs.shape
-
-    labels = inputs[3]
-
+    perm_inputs, feat_inputs, labels = vectorize(args["good_path"], args["mal_path"])
     perm_width = len(perm_inputs[0])
     feat_width = len(feat_inputs[0])
-    comb_width = len(comb_inputs[0])
 
-    print perm_inputs.shape
-    print feat_inputs.shape
-    print labels.shape
+    grid_search(args, perm_inputs, feat_inputs, labels)
+    return
+    #comb_width = len(comb_inputs[0])
 
-    #kfolds cross validation
-    skf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
-    sss = StratifiedShuffleSplit(n_splits=5, random_state=42, test_size=.2)
-    feat_split = sss.split(feat_inputs, labels)
-    for train_index, test_index in feat_split:
-        model = Multi_model(perm_width, feat_width)
-        #model2 = create_one_layer(comb_width)
-        model.compile(loss='binary_crossentropy', optimizer='nadam', metrics=['accuracy'])
-        perm_train, perm_test = perm_inputs[train_index], perm_inputs[test_index]
-        feat_train, feat_test = feat_inputs[train_index], feat_inputs[test_index]
-        comb_train, comb_test, = comb_inputs[train_index], comb_inputs[test_index]
-        labels_train, labels_test = labels[train_index], labels[test_index]
-        model.fit([perm_train, feat_train], labels_train, epochs=16, batch_size=10)
-        #model2.fit(comb_train, labels_train, epochs=10, batch_size=16)
-        print ''
-        print model.evaluate([perm_test, feat_test], labels_test)
-        #print model2.evaluate(comb_test, labels_test)
-        print ''
+    avg = 0
+    for x in split_acc:
+        avg+=x
+    print 'avg acc is :' + str(avg/5)
 
     return
 
@@ -80,7 +58,7 @@ def vectorize(good_path, mal_path):
 
     perm_vect = CountVectorizer(analyzer=partial(regexp_tokenize, pattern=perm_pattern))
     feat_vect = CountVectorizer(analyzer=partial(regexp_tokenize, pattern=feat_pattern))
-    comb_vect = CountVectorizer(analyzer=partial(regexp_tokenize, pattern=comb_pattern))
+    #comb_vect = CountVectorizer(analyzer=partial(regexp_tokenize, pattern=comb_pattern))
 
     time0 = timeit.default_timer()
     perm_inputs_sparse = perm_vect.fit_transform(samples)
@@ -91,47 +69,228 @@ def vectorize(good_path, mal_path):
     feat_inputs_dense = feat_inputs_sparse.todense()
     feat_inputs = np.array(feat_inputs_dense)
 
-    comb_inputs_sparse = comb_vect.fit_transform(samples)
-    comb_inputs_dense = comb_inputs_sparse.todense()
-    comb_inputs = np.array(comb_inputs_dense)
+    #comb_inputs_sparse = comb_vect.fit_transform(samples)
+    #comb_inputs_dense = comb_inputs_sparse.todense()
+    #comb_inputs = np.array(comb_inputs_dense)
 
-    print perm_vect.get_feature_names()
-    inputs = [perm_inputs, feat_inputs, comb_inputs, labels]
-    return inputs
+    return perm_inputs, feat_inputs, labels
 
-
-
-
-def Multi_model(perm_width, feat_width):
-    perm_input = Input(shape=(perm_width,), name='permissions_input')
-    x = Dense(32, activation='relu')(perm_input)
-    x = Dropout(.2)(x)
-    x = Dense(32, activation='relu')(perm_input)
-
-    feat_input = Input(shape=(feat_width,), name='features_input')
-    #y = Dense(16, activation='relu')(feat_input)
-    x = concatenate([x, feat_input])
-    x = Dense(16, activation='relu')(x)
-    x = Dropout(.1)(x)
-    x = Dense(16, activation='relu')(x)
-
-    output = Dense(1, activation='sigmoid', name="output")(x)
-
-    model = Model(inputs=[perm_input, feat_input], outputs=output)
-    print model.summary()
-    return model
-
-def create_one_layer(perm_width, dropout_rate=.1, neurons=32, optimizer='nadam'):
-    #baseline Model
-    model = Sequential()
-    #The first param in Dense is the number of neurons in the first hidden layer
-    model.add(Dense(25, input_dim=perm_width, kernel_initializer='normal', activation='relu',))
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
-    # Compile model
-    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    return model
+def grid_search(args, perm_inputs, feat_inputs, labels):
+    '''
+    The below method is a modified implementation of the gridsearch method in
+    KTFBinClass.py that manually iterates through all params via nested loops
+    this method has been created to allow for multi_input neural networks
+    '''
+    perm_width = int(len(perm_inputs[0]))
+    feat_width = int(len(feat_inputs[0]))
+    print type(perm_width)
+    input_ratios = args["input_ratio"]
+    splits = args["splits"]
+    epochs = args["epochs"]
+    batch_size = args["batch_size"]
+    neurons = args["neurons"]
+    modelName = args["model"]
+    print modelName
 
 
-if __name__=="__main__":
+
+    for m in modelName:
+        for r in args["train_ratio"]:
+            percent=float(r)/100
+            sss = StratifiedShuffleSplit(n_splits=5, random_state=0, test_size=percent)
+            for ir in input_ratios:
+                for epoch in epochs:
+                    for batch in batch_size:
+                        for size in neurons:
+                            print str(r) + ' ' + str(epoch) + ' ' + str(batch)
+                            for train_index, test_index in sss.split(perm_inputs, labels):
+                                print m
+                                if m == "oneLayer":
+                                    print 'here'
+                                    model = create_one_layer(optimizer='nadam', data_width=perm_width)
+                                elif m == "dual_simple":
+                                    model = create_dualInputSimple(input_ratio=.125, perm_width=perm_width, feat_width=feat_width)
+                                elif m == "dual_large":
+                                    model = create_dualInputLarge(dropout_rate=.1, input_ratio=.125, perm_width=perm_width, feat_width=feat_width)
+
+                                perm_train, perm_test = perm_inputs[train_index], perm_inputs[test_index]
+                                feat_train, feat_test = feat_inputs[train_index], feat_inputs[test_index]
+                                labels_train, labels_test = labels[train_index], labels[test_index]
+                                if m != "oneLayer":
+                                    print "multi_input: " + str(m)
+                                    model.fit([perm_train, feat_train], labels_train, epochs=epoch, batch_size=batch)
+                                    labels_pred = model.predict([perm_test, feat_test], batch_size=batch)
+
+                                elif m == "oneLayer":
+                                    print "single_input: " + str(m)
+                                    model.fit(perm_train, labels_train, epochs=epoch, batch_size=batch)
+                                    labels_pred = model.predict(perm_test, batch_size=batch)
+
+                                labels_pred = (labels_pred > 0.5)
+                                cm = confusion_matrix(labels_test, labels_pred)
+                                print "acc: " + str(float((cm[0][0] + cm[1][1]))/len(labels_pred))
+
+
+
+
+                                #split_acc = np.append(split_acc, model.evaluate([perm_test, feat_test], labels_test)[1])
+                                #comb_train, comb_test, = comb_inputs[train_index], comb_inputs[test_index]
+                                #model.fit(comb_train, labels_train, epochs=8, batch_size=64)
+                                #split_acc = np.append(split_acc, model.evaluate(comb_test, labels_test)[1])
+
+    return
+
+
+    print("%s Best: %f using %s" % (modelName, grid_fit.best_score_, grid_fit.best_params_))
+
+    d = datetime.datetime.today()
+    month = str( '%02d' % d.month)
+    day = str('%02d' % d.day)
+    hour = str('%02d' % d.hour)
+    min = str('%02d' % d.minute)
+
+    df = pandas.DataFrame(grid_fit.cv_results_)
+    try:
+        path1 = '/home/lab309/pythonScripts/testResults/deep_results/gridSearch' + modelName + month + day + hour + min + '.csv'
+        file1 = open(path1, "w+")
+    except:
+        path1 = "gridSearch" + modelName + ".csv"
+        file1 = open(path1, "w+")
+    df.to_csv(file1, index=True)
+    file1.close()
+
+    return 0
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-gp", "--good_path", help="Good File Path")
+    parser.add_argument("-mp", "--mal_path", help="Malware File Path")
+    parser.add_argument("-ad", "--adverse", help="Turns on Adversarial Learning")
+    parser.add_argument("-m", "--mode", help="Choose mode: full, grid")
+    parser.add_argument("-e", "--epochs", help="Number of Epochs, can be list for grid search", type=int, nargs="*")
+    parser.add_argument("-tr", "--train_ratio", nargs="*", type=int,
+                        help="Set Test Ratios. Enter as a percent (20,40,60,80). Can be a list space delimited")
+    parser.add_argument("-bs", "--batch_size", nargs="*", type=int,
+                        help="Batch size. Can be a list space delimited")
+    parser.add_argument("-n", "--neurons", nargs="*", type=int,
+                        help="Number of Neurons. Can be a list space delimited")
+    parser.add_argument("-o", "--optimizer", nargs="*",
+                        help="Optimizers. Can be a list space delimited")
+    parser.add_argument("-w", "--weight_constraint", nargs="*", type=int,
+                        help="Weight Constraint. Can be a list space delimited")
+    parser.add_argument("-d", "--dropout", nargs="*", type=int,
+                        help="Dropout. Enter as percent (10,20,30,40...). Can be a list space delimited.")
+    parser.add_argument("-model", "--model", help="Select which model to run: all \
+    , one_layer, four_decr, four_same")
+    parser.add_argument("-s", "--splits", help="Number of Splits for SSS", type=int)
+    parser.add_argument("-ir", "--input_ratio", help="ratio of layer width between \
+     features and permissions layers", type=float, nargs="*")
+
+    args = parser.parse_args()
+
+    arguments = {}
+
+    if args.good_path:
+        good_path = args.good_path
+        arguments["good_path"] = good_path
+    else:
+        print("Needs Good Path with -gp or --good_path")
+        sys.exit()
+
+    if args.mal_path:
+        mal_path = args.mal_path
+        arguments["mal_path"] = mal_path
+    else:
+        print("Needs Malware Path with -mp or --mal_path")
+        sys.exit()
+
+    if args.adverse:
+        adverse = True
+    else:
+        adverse = False
+    arguments["adverse"] = adverse
+
+    if args.mode == "grid":
+        mode = "grid"
+        print("Mode is %s" % mode)
+    else:
+        mode = "full"
+        print("Mode is %s" % mode)
+    arguments["mode"] = mode
+
+    if args.model == "all":
+        model = ["oneLayer", "dual_simple", "dual_large"]
+    elif args.model in ["oneLayer", "fourDecr", "fourSame", "dual_inputLarge", "dual_inputSimple"]:
+        model = [args.model]
+    else:
+        print("Defaulting to All models")
+        model = ["oneLayer", "fourDecr", "fourSame"]
+    arguments["model"] = model
+
+    if args.epochs:
+        epochs = args.epochs
+    else:
+        print("Defaulting to 16 epochs")
+        epochs = [16]
+    arguments["epochs"] = epochs
+    if args.train_ratio:
+        train_ratio = args.train_ratio
+    else:
+        print("Defaulting to testing all ratios")
+        train_ratio = [20, 40, 60, 80]
+    arguments["train_ratio"] = train_ratio
+
+    if args.batch_size:
+        batch_size = args.batch_size
+    else:
+        print("Defaulting to Batch Size 10")
+        batch_size = [10]
+    arguments["batch_size"] = batch_size
+
+    if args.neurons:
+        neurons = args.neurons
+    else:
+        print("Defaulting to 45 Neurons")
+        neurons = [45]
+    arguments["neurons"] = neurons
+
+    if args.optimizer:
+        optimizer = args.optimizer
+    else:
+        print("Defaulting to NADAM Optimizer")
+        optimizer = "Nadam"
+    arguments["optimizer"] = optimizer
+
+    if args.weight_constraint:
+        weight_constraint = args.weight_constraint
+    else:
+        print("Defaulting to weight constraint 5")
+        weight_constraint = [5]
+    arguments["weight_constraint"] = weight_constraint
+
+    if args.dropout:
+        dropout = args.dropout
+    else:
+        print("Defaulting to dropout of 10%")
+        dropout = [10]
+    arguments["dropout"] = dropout
+
+    if args.splits:
+        splits = args.splits
+    else:
+        print("Defaulting to 1 SSS Split")
+        splits = [1]
+    arguments["splits"] = splits
+
+    if args.input_ratio:
+        input_ratio = args.input_ratio
+    else:
+        print("default to .25 input ratio")
+        input_ratio = [.25]
+    arguments["input_ratio"] = input_ratio
+
+    return arguments
+
+
+if __name__ == "__main__":
     main()
--- Classification/keras_models.py
+++ Classification/keras_models.py
@@ -1,14 +1,14 @@
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.models import Sequential, Model
 from keras.constraints import maxnorm
+from keras.layers import Merge, Dense, Dropout, Input, concatenate
+from keras.optimizers import Nadam
 
-
-
-def create_one_layer(neurons=25, optimizer='adam', dropout_rate=0.0, weight_constraint=0):
+def create_one_layer(data_width, neurons=25, optimizer='adam', dropout_rate=0.0, weight_constraint=0):
     #baseline Model
     model = Sequential()
     #The first param in Dense is the number of neurons in the first hidden layer
-    model.add(Dense(neurons, input_dim=22300, kernel_initializer='normal', activation='relu',kernel_constraint=maxnorm(weight_constraint) ))
+    #model.add(Dense(neurons, input_dim=22300, kernel_initializer='normal', activation='relu',kernel_constraint=maxnorm(weight_constraint) ))
+    model.add(Dense(neurons, input_dim=data_width, kernel_initializer='normal', activation='relu'))
     model.add(Dropout(dropout_rate))
     model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
     # Compile model
@@ -65,4 +65,34 @@ def create_binaryDecrease(neurons=25, optimizer='adam'):
     model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    return model
+
+def create_dualInputSimple(input_ratio, feat_width, perm_width, neurons=32, dropout_rate=0.1):
+    '''this simple model performs no additional analysis after concatenation'''
+    perm_input = Input(shape=(perm_width,), name='permissions_input')
+    x = Dense(neurons, activation='relu')(perm_input)
+    feat_input = Input(shape=(feat_width,), name='features_input')
+    y = Dense(int(neurons*input_ratio), activation='relu')(feat_input)
+    x = concatenate([x, y])
+    output = Dense(1, activation='sigmoid', name='output')(x)
+    model = Model(inputs=[perm_input, feat_input], outputs=output)
+    model.compile(loss='binary_crossentropy', optimizer='nadam', metrics=['accuracy'])
+    return model
+
+def create_dualInputLarge(input_ratio, feat_width, perm_width, neurons=32, dropout_rate=0.1):
+    '''this model performs additional analysis with layers after concatenation'''
+    perm_width=int(perm_width)
+    perm_input = Input(shape=(perm_width,), name='permissions_input')
+    x = Dense(neurons, activation='relu')(perm_input)
+    x = Dropout(dropout_rate)(x)
+    x = Dense(neurons, activation='relu')(x)
+    feat_input = Input(shape=(feat_width,), name='features_input')
+    y = Dense(int(neurons*input_ratio), activation='relu')(feat_input)
+    x = concatenate([x, y])
+    x = Dense(int((neurons+(neurons*input_ratio))/2), activation='relu')(x)
+    x = Dropout(dropout_rate)(x)
+    x = Dense(int((neurons+(neurons*input_ratio))/2), activation='relu')(x)
+    output = Dense(1, activation='sigmoid', name="output")(x)
+    model = Model(inputs=[perm_input, feat_input], outputs=output)
+    model.compile(loss='binary_crossentropy', optimizer='nadam', metrics=['accuracy'])
     return model
